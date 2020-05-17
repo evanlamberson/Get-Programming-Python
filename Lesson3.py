17800ad1d3f8737f104c0859a57c55ef5a3c310b
Evan Lamberson <evan.lamberson.99@gmail.com>
2020-05-16 22:01:28 -0400

    actually added it this time

---
+++ Lesson3.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 16 21:06:43 2020
+
+@author: evanlamberson
+"""
+# This code doesn't show on the console
+3 + 2
+4 / 5
+
+# This code displays an output in the console
+print(3 + 2)
+print(4 / 5)
---
+++ Lesson4.5.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 16 21:08:41 2020
+
+@author: evanlamberson
+"""
+#Quick Check 4.5
+apples = 5
+oranges = 10
+fruits = apples + oranges
+apples = 20
+fruits = apples + oranges
